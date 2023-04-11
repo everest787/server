39468ae79ac07a57d919be7e4de24c6d5fe8e746
Chelsea <chelseaeverest7@gmail.com>
2023-04-11 17:46:22 -0400

    Debugging

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export PATH=$PATH:/usr/local/bin
+# Output current working directory to a file
+pwd > /tmp/install_dependencies_dir.log
 
+# Install Node.js dependencies
 cd /home/ec2-user/server/
 npm install
