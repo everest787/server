287155a973f1da2d77154b030e83474df2f45453
Chelsea <chelseaeverest7@gmail.com>
2023-04-11 12:26:38 -0400

    Testing Server

--- index.js
+++ index.js
@@ -1,44 +1,18 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const port = 3001;
-
-let rawblog = fs.readFileSync('./content/blog.json');
-let blog = JSON.parse(rawblog);
-
-let rawprojects = fs.readFileSync('./content/projects.json');
-let projects = JSON.parse(rawprojects);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get("/blog", (req, res) => {
-  const sortedBlogs = blog.blogs;
-  sortedBlogs.sort((a, b) => new Date(b.date) - new Date(a.date))
-  res.json({ data: sortedBlogs });
-});
-
-app.get("/blog/:title", (req, res) => {
+app.get("/test/:title", (req, res) => {
   const title = req.params.title;
-  const data = blog.blogs.filter(
-    function(data){ return data.title.replace(/ /g,'') == title }
-  );
-  res.json(data[0]);
-});
-
-app.get("/projects", (req, res) => {
-  res.json({ data: projects.projects });
-});
-
-app.get("/projects/:title", (req, res) => {
-  const title = req.params.title;
-  const data = projects.projects.filter(
-    function(data){ return data.title.replace(/ /g,'') == title }
-  );
-  res.json(data[0]);
+  res.json({
+    message: "Got a response: " + title
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at port ${port}`);
 });
