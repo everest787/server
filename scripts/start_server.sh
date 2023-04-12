c450ff51c7e52603ebcb327c3f39447758cf38d7
Chelsea <chelseaeverest7@gmail.com>
2023-04-12 16:45:13 -0400

    Run the process in the background

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,4 +4,14 @@
 cd /home/ec2-user/server
 npm install
 
-node index.js
+#starts the server in the background, saves its process ID
+node index.js &
+PID=$!
+
+# loop that checks if the server is reachable and will keep running until the server is up
+while ! curl -s http://localhost:3000 >/dev/null; do
+    sleep 1
+done
+
+# exit with a message indicating the server's process ID.
+echo "Server is running with PID ${PID}."
