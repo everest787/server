bdb1801fe6516d7b224ca31ed63ef84a1c3a4287
Chelsea <chelseaeverest7@gmail.com>
2023-04-12 16:05:03 -0400

    trying something else?

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 # Install Node.js dependencies
-cd /home/ec2-user
-rm -r server
-git clone https://github.com/everest787/server.git
-cd server
-npm install
+cd /home/ec2-user/server
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /home/ec2-user/server
+npm install
 node index.js
